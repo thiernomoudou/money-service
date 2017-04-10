16c89fe9aaf70e3ee7ef00a0991803e81b3c9f22
thiernomoudou <souleymanemoudou@gmail.com>
2017-04-10 16:39:11 +0000

    create my login page

--- myproject/transfer_app/urls.py
+++ myproject/transfer_app/urls.py
@@ -9,5 +9,6 @@ from . import views
 
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    url(r'^$', views.login_view, name='login'),
+    url(r'^home/$', views.home, name='home'),
 ]
--- myproject/transfer_app/views.py
+++ myproject/transfer_app/views.py
@@ -1,10 +1,32 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
 
-# Create your views here.
+from django.contrib.auth import authenticate, login, logout
+from .forms import LoginForm
 
-def index(request):
-    return HttpResponse('Hello there')
+
+def home(request):
+    return render(request, 'home.html')
+
+def login_view(request):
+    form = LoginForm()
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            u = form.cleaned_data['username']
+            p = form.cleaned_data['password']
+            user = authenticate(username = u, password = p)
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    return redirect('home')
+                else:
+                    print("the account has been disabled")
+            else:
+                print("The username and password were incorrect")
+
+    else:
+        form = LoginForm()
+        return render(request, 'login.html', {'form': form})
