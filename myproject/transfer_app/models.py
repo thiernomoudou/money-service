fbda3eb8c986951f05bc7bcfd724e2ce3d04c418
thiernomoudou <souleymanemoudou@gmail.com>
2017-04-13 13:23:45 +0000

    add some work

--- myproject/transfer_app/models.py
+++ myproject/transfer_app/models.py
@@ -1,10 +1,12 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+import datetime
 
 from django.db import models
 
 
 class TransactionModel(models.Model):
+    date = models.DateTimeField(editable=False)
     origin = models.CharField(max_length=50)
     destination = models.CharField(max_length=50)
     sender_name = models.CharField(max_length=50)
@@ -14,3 +16,11 @@ class TransactionModel(models.Model):
     receiver_phone = models.CharField(max_length=15)
     amount = models.DecimalField(max_digits=15, decimal_places=2)
     currency = models.CharField(max_length=10)
+
+    def __str__(self):
+        return(self.receiver_name)
+
+    def save(self):
+        if not self.id:
+            self.date = datetime.date.today()
+        super(TransactionModel, self).save()
