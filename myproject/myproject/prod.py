7152165db7b97784c3473bafe4fbf94363a72f9d
thiernomoudou <souleymanemoudou@gmail.com>
2017-04-28 15:51:02 +0000

    add production settings

---
+++ myproject/myproject/prod.py
@@ -0,0 +1,17 @@
+import os
+from myproject.settings import *
+
+ALLOWED_HOSTS = [ 'test.tsdiallo.com', '207.154.254.143']
+DEBUG = False
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ['NAME'],
+        'USER': os.environ['USER'],
+        'PASSWORD': os.environ['PASSWORD'],
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
